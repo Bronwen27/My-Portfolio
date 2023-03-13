d2baa7c7257e185211939fe2b9e81ca65b9cf50e
Bronwen27 <118174027+Bronwen27@users.noreply.github.com>
2023-03-13 19:06:38 +0000

    completed info details

--- src/componants/Homepage.js
+++ src/componants/Homepage.js
@@ -2,11 +2,19 @@
 function Homepage() {
     return (
       <div>
-     <p>Welcome to the world of... <br>Bronwen Jenner</br></p>
+     <p>Welcome to the world of... <span> Bronwen Jenner</span></p>
      <p>Front-End Web Developer</p>
      <p>Take a dive into my latest coding projects using:</p>
      <ul>
-        <li>JavaScript</li>
+        <ul>JavaScript</ul>
+        <ul>React</ul>
+        <ul>JQuery</ul>
+        <ul>ES6</ul>
+        <ul>Node</ul>
+        <ul>APIs</ul>
+        <ul>UX & CSS</ul>
+        <ul>HTML</ul>
+        <ul>Git</ul>
       </ul>
       </div>
     );
