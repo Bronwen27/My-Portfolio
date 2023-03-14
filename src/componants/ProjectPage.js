8c8810b705e4fefb98c5f7335ecfe6502a0b6448
Bronwen27 <118174027+Bronwen27@users.noreply.github.com>
2023-03-14 01:47:36 +0000

    added projectpage componant

---
+++ src/componants/ProjectPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Project = ({ project }) => {
+  const { id, title, deployedLink, githubLink, image } = project;
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <Link to={`/projects/${id}`}>
+        <img src={image} alt={`${title} screenshot`} />
+      </Link>
+      <p>Deployed version: <a href={deployedLink}>{deployedLink}</a></p>
+      <p>GitHub repository: <a href={githubLink}>{githubLink}</a></p>
+    </div>
+  );
+};
+
+export default Project;
